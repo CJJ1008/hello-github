21c650e08331c075261383c4eeb79fde2c202e9f
CJJ1008 <843515223@qq.com>
2026-01-06 08:19:11 +0000

    init: hello-github demo

---
+++ cjj/main.py
@@ -0,0 +1,12 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def main() -> None:
+    print("Hello, GitHub!")
+    print("2 + 3 =", add(2, 3))
+
+
+if __name__ == "__main__":
+    main()
+
